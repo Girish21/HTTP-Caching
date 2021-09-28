3a717d80b928d4decc38ff2ff49278b2e3d87123
Girish21 <girishkumarv21@gmail.com>
2021-09-28 23:36:46 +0530

    to dark mode

--- cdn/src/index.js
+++ cdn/src/index.js
@@ -6,7 +6,7 @@ express.get("/", async (_, res) => {
   await new Promise((resolve) => setTimeout(resolve, 5000));
 
   let html =
-    '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Caching</title></head><body>';
+    '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta name="color-scheme" content="dark"><title>Caching</title></head><body>';
 
   html += "<h1>Hello from server!!!</h1><ul>";
 
--- header/src/index.js
+++ header/src/index.js
@@ -2,7 +2,7 @@ const http = require("http");
 const crypto = require("crypto");
 
 const HTML =
-  '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0">';
+  '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta name="color-scheme" content="dark">';
 
 const generateJunk = (html) => {
   html += "<main><ul>";
