b4a15aeadd5387ac585ef5298d1bb6a2eaa6f1ce
Girish21 <girishkumarv21@gmail.com>
2021-09-28 23:30:41 +0530

    updated CDN example

--- cdn/src/index.js
+++ cdn/src/index.js
@@ -3,15 +3,27 @@ const express = require("express")();
 express.get("/", async (_, res) => {
   console.log("🛠 Server wroking");
 
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
   let html =
     '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Caching</title></head><body>';
 
-  html += "<h1>Hello from server!!!</h1>";
-  html += "</body></html>";
+  html += "<h1>Hello from server!!!</h1><ul>";
+
+  for (let i = 0; i < 1000; i++) {
+    html += `<li>Item ${i + 1}</li>`;
+  }
+
+  html += "</ul></body></html>";
+
+  console.log("✨ Server - Done");
 
   res
     .status(200)
-    .setHeader("Cache-Control", "max-age=5, s-maxage=20")
+    .setHeader(
+      "Cache-Control",
+      "max-age=5, s-maxage=20, stale-while-revalidate=20"
+    )
     .setHeader("Content-Type", "text/html; charset=utf-8")
     .send(html)
     .end();
